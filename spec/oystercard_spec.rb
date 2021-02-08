1c41313135bfe490598d4b7a1aca3baa35f7132b
Rorie Clarke <Rorie@MacBook-Pro.local>
2021-02-08 15:53:51 +0000

    added top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,12 @@ describe Oystercard do
     expect(subject.balance).to eq(0)
   end
 
+  describe '#top_up' do
+    it 'Card has top-up facility' do
+      expect(subject).to respond_to(:top_up).with(1).argument
+    end
+    it 'User can add money to the card' do
+      expect{subject.top_up(10)}.to change{subject.balance}.from(0).to(10)
+    end
+  end
 end
