ac4afea2fd09f0b549a3518c126b5ed1b1d6c575
Rorie Clarke <Rorie@MacBook-Pro.local>
2021-02-08 16:49:43 +0000

    Created Deduct method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,15 +1,20 @@
 class Oystercard
   attr_reader :balance
 
+  DEFAULT_BALANCE = 0
   MAXIMUM_BALANCE = 90
 
   def initialize
-    @balance = 0
+    @balance = DEFAULT_BALANCE
   end
 
   def top_up(cash)
     fail "Warning: top-up amount will exceed maximum balance of £#{MAXIMUM_BALANCE}" if (@balance + cash) > MAXIMUM_BALANCE
     @balance += cash
+  end
+
+  def deduct(cash)
+    @balance -= cash
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,7 +3,7 @@ require 'oystercard'
 describe Oystercard do
 
   it 'oystercard has a balance of 0' do
-    expect(subject.balance).to eq(0)
+    expect(subject.balance).to eq Oystercard::DEFAULT_BALANCE
   end
 
   describe '#top_up' do
@@ -11,13 +11,19 @@ describe Oystercard do
       expect(subject).to respond_to(:top_up).with(1).argument
     end
     it 'User can add money to the card' do
-      expect{subject.top_up(10)}.to change{subject.balance}.from(0).to(10)
+      expect{subject.top_up(10)}.to change{subject.balance}.by 10
     end
     it 'User cannot top up more than card maximum' do
       max_balance = Oystercard::MAXIMUM_BALANCE
       subject.top_up(max_balance)
       expect{subject.top_up(1)}.to raise_error("Warning: top-up amount will exceed maximum balance of £#{max_balance}")
     end
+  end
 
+  describe '#deduct'do
+    it 'Card deducts cash from balance' do
+      subject.top_up(10)
+      expect{subject.deduct(10)}.to change{subject.balance}.by -10
+    end
   end
 end
